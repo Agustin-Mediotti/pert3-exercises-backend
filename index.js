d86772f6feebbbd448630c57fa7535eeb321d286
NetCreature <agustinmediotti@gmail.com>
2023-09-05 12:39:46 -0300

    P03 E3.2

--- index.js
+++ index.js
@@ -26,8 +26,35 @@ let notes = [
   },
 ];
 
+const getDateTime = () => {
+  let time = new Date();
+  let dateTime = `${new Intl.DateTimeFormat("en-US", {
+    weekday: "long",
+    month: "long",
+    day: "2-digit",
+  })
+    .format(new Date(Date.UTC(time.getDay(), time.getMonth())))
+    .split(" ")
+    .map((w) => w.substring(0, 3))
+    .join(
+      " "
+    )} ${time.getFullYear()} ${time.getHours()}:${time.getMinutes()}:${time.getSeconds()} GMT+${
+    time.getTimezoneOffset() / 60
+  } (${Intl.DateTimeFormat().resolvedOptions().timeZone.split("/").join(" ")})`;
+
+  return dateTime;
+};
+
 app.get("/api/persons", (req, res) => {
   res.json(notes);
+});
+
+app.get("/info", (req, res) => {
+  res.send(
+    `<p>Phonebook has info for ${
+      notes.length
+    } people <br/> ${getDateTime()}</p>`
+  );
 });
 
 app.listen(PORT, () => {
